e74487833713a7fe9f934cf2854c05e9cd9eb823
MUSTAPHA-MOUZOUE <mostafa.abiou@gmail.com>
2023-12-22 14:26:33 +0100

    push

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              const morgan = require("morgan");
 
 dotenv.config({ path: "config.env" });
 const dbConnection = require("./config/db");
