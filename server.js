7b48e08c211ecc525304335093132e721adddf23
MUSTAPHA-MOUZOUE <mostafa.abiou@gmail.com>
2023-12-22 13:02:19 +0100

    push

--- server.js
+++ server.js
@@ -1,18 +1,2 @@
 const express = require("express");
 const app = express();
-const port = 3001;
-const mongoose = require("mongoose");
-app.use(express.urlencoded({ extended: true }));
-
-mongoose
-  .connect(`mongodb://localhost:37017`)
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`http://localhost:${port}/`);
-    });
-  })
-
-  .catch((err) => {
-    console.log(err);
-  });
-app.listen(port)
